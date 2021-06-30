20080ef6b3455bc64c4ba66ae640ae9c84cfd7b1
Ryan <54348153+thinhlesdev@users.noreply.github.com>
2021-06-30 17:24:46 +0700

    finish basic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import './styles.css';
+import './style.scss';
+
 console.log('Webpack 5 is here!!!');
+
+const testFunc = () => {
+  return [1, 2];
+};
+
+const [a, b] = testFunc();
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, 'src', 'index.js') },
+  output: { path: path.resolve(__dirname, 'dist') },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+};
